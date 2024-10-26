95c9605bbe31f287abb376717be73128fedccc39
357-22 <vika.p.2006@bk.ru>
2024-10-26 11:38:02 +0300

    Готовое решение

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Введите элементы первого массива (через пробел):")
+    val firstArray = readLine()?.split(" ")?.map { it.toInt() } ?: return
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println("Введите элементы второго массива (через пробел):")
+    val secondArray = readLine()?.split(" ")?.map { it.toInt() } ?: return
+
+    val intersection = findIntersection(firstArray, secondArray)
+    println("Пересечение массивов: $intersection")
+}
+fun findIntersection(arr1: List<Int>, arr2: List<Int>): List<Int> {
+    val countMap1 = mutableMapOf<Int, Int>()
+    val countMap2 = mutableMapOf<Int, Int>()
+
+    for (num in arr1) {
+        countMap1[num] = countMap1.getOrDefault(num, 0) + 1
     }
+    for (num in arr2) {
+        countMap2[num] = countMap2.getOrDefault(num, 0) + 1
+    }
+
+    val result = mutableListOf<Int>()
+    for (key in countMap1.keys) {
+        if (countMap2.containsKey(key)) {
+            val minCount = minOf(countMap1[key]!!, countMap2[key]!!)
+            result.addAll(List(minCount) { key })
+        }
+    }
+    return result
 }
